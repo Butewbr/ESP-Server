1b8e59f0788d38412c84b2ba270bd11ae323c29d
Bernardo Pandolfi Costa <bernardopandolficosta@gmail.com>
2023-12-06 16:08:49 -0300

    SERVER WORKING

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,83 @@
 #include <WiFi.h>
-
+#include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 
 // Replace with your network credentials
-const char* ssid = "GIGA SERVER";
-const char* password = "123quatrocincoseis";
+const char *ssid = "GIGA SERVER";
+const char *password = "123quatrocincoseis";
+
+void listenServer(void *parameter);
+void printMessage(void *parameter);
 
 // Set web server port number to 80
-AsyncWebServer server(80);
+WiFiServer server(80);
+SemaphoreHandle_t mutex;
 
-// Variable to store the HTTP request
-String header;
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println();
 
-// Auxiliar variables to store the current output state
-String output26State = "off";
-String output27State = "off";
+    // Setting the ESP as an access point
+    Serial.print("Setting AP (Access Point)...\n");
+    // Remove the password parameter, if you want the AP (Access Point) to be open
+    WiFi.softAP(ssid, password);
 
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  
-  // Setting the ESP as an access point
-  Serial.print("Setting AP (Access Point)…");
-  // Remove the password parameter, if you want the AP (Access Point) to be open
-  WiFi.softAP(ssid, password);
+    Serial.print("Inicializando servidor...\n");
+    server.begin();
 
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
+    Serial.print("Mutex sendo criado...\n");
+    mutex = xSemaphoreCreateBinary();
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial.print("LETS GET IT, CONEXÃO ACONTECEU!!\n");
-    request->send_P(200, "text/plain", "eae bobao kakakaka");
-  });
+    Serial.print("Criando tarefa listenServer...\n");
+    xTaskCreate(listenServer, "listenServer", 10000, NULL, 2, NULL);
+    xTaskCreate(printMessage, "printMessage", 10000, NULL, 2, NULL);
 
-  server.begin();
+    // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    //     Serial.print("Conexão GET recebida.\n");
+    //     request->send_P(200, "text/plain", "eae bobao kakakaka");
+    //     // xSemaphoreGive(mutex);
+    // });
+
+    // server.on("/", HTTP_POST, [](AsyncWebServerRequest *request){
+    //     if (request->hasParam("body", true)) {
+    //         AsyncWebParameter* p = request->getParam("body", true);
+    //         String message = p->value();
+    //         Serial.print(message); // Print the message from the POST request
+    //     }
+    //     Serial.print("Conexão POST recebida.\n");
+    //     request->send(200, "text/plain", "RECEBI VEI!!!");
+    // });
 }
 
-void loop(){
+void listenServer(void *parameter) {
+    for(;;) {
+        WiFiClient client = server.available();
+        Serial.print("Aguardando um cliente...\n");
+        
+        if(client && client.connected()) {
+            char c = client.read();
 
+            client.print(200);
+            
+            client.stop();
+            xSemaphoreGive(mutex);
+        }
+        vTaskDelay(1000);
+    }
 }
 
+void printMessage(void *parameter) {
+    for(;;) {
+        xSemaphoreTake(mutex, portMAX_DELAY);
+        Serial.print("Conexão Recebida.\n");
+    }
+}
+
+void loop()
+{
+}
